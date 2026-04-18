c80133e0dd2be21db1dacca341ffe258aab6a72c
Manveer Dhanjal <manveerdhanjal2003@gmail.com>
2026-04-18 17:40:51 -0500

    Fixed profile

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,38 @@ import {
 } from "@aws-amplify/ui-react";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
+import { getCurrentUser } from "aws-amplify/auth";
 import "@aws-amplify/ui-react/styles.css";
 import { generateClient } from "aws-amplify/data";
 import outputs from "../amplify_outputs.json";
-/**
- * @type {import('aws-amplify/data').Client<import('../amplify/data/
-resource').Schema>}
- */
+
 Amplify.configure(outputs);
+
 const client = generateClient({
   authMode: "userPool",
 });
+
 export default function App() {
   const [userprofiles, setUserProfiles] = useState([]);
   const { signOut } = useAuthenticator((context) => [context.user]);
+
   useEffect(() => {
+    async function fetchUserProfile() {
+      const { data: profiles } = await client.models.UserProfile.list();
+
+      if (profiles.length === 0) {
+        const userInfo = await getCurrentUser();
+        const { data: newProfile } = await client.models.UserProfile.create({
+          email: userInfo.signInDetails?.loginId,
+        });
+        setUserProfiles([newProfile]);
+      } else {
+        setUserProfiles(profiles);
+      }
+    }
     fetchUserProfile();
   }, []);
-  async function fetchUserProfile() {
-    const { data: profiles } = await client.models.UserProfile.list();
-    setUserProfiles(profiles);
-  }
+
   return (
     <Flex
       className="App"
